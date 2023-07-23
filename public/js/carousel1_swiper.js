ec19354e60881a45e1e780b5e09f35eac6f88e1d
Giselda  Arevalo <96034020+giselarevalo@users.noreply.github.com>
2023-07-23 17:37:20 -0300

    Styled Carousel 1 and categories section with CSS

--- public/js/carousel1_swiper.js
+++ public/js/carousel1_swiper.js
@@ -3,9 +3,9 @@ var swiper = new Swiper(".mySwiper1", {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
-    /* autoplay: {
+    autoplay: {
         delay: 3000, // Tiempo en milisegundos entre cada cambio de slide
         disableOnInteraction: false, // Permite que el autoplay continúe incluso si el usuario interactúa con el slider
       },
-    loop: true, */ // Permite que el slider se desplace de forma continua y se repita al llegar al último slide
+    loop: true, // Permite que el slider se desplace de forma continua y se repita al llegar al último slide
   });
