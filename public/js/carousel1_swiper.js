4ad36c3970511e2e4517226628eb88dff040a531
Giselda  Arevalo <96034020+giselarevalo@users.noreply.github.com>
2023-07-23 01:03:04 -0300

    created html/css index structure

---
+++ public/js/carousel1_swiper.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".mySwiper1", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    /* autoplay: {
+        delay: 3000, // Tiempo en milisegundos entre cada cambio de slide
+        disableOnInteraction: false, // Permite que el autoplay continúe incluso si el usuario interactúa con el slider
+      },
+    loop: true, */ // Permite que el slider se desplace de forma continua y se repita al llegar al último slide
+  });
