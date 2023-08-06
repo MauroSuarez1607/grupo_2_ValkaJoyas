e69cfc9daf4be8e607e4c4d59a55a3f70e82109b
Giselda  Arevalo <96034020+giselarevalo@users.noreply.github.com>
2023-08-06 12:56:28 -0300

    Created usersController file

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const indexController = require('../Controllers/indexController')
 
-/* GET home page. */
+/* / */
 router.get('/', indexController.index);
 
 module.exports = router;
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,9 +1,12 @@
-var express = require('express');
-var router = express.Router();
+const  express = require('express');
+const  router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const usersController = require('../Controllers/usersController');
+
+
+
+/* /users */
+router.get('/login', usersController.login );
+router.get('/register', usersController.register );
 
 module.exports = router;
