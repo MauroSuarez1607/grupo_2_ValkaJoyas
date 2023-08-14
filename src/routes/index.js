58a448a44a8871ba6903e7ac50676987ea1358f4
Giselda  Arevalo <96034020+giselarevalo@users.noreply.github.com>
2023-08-13 22:03:11 -0300

    Add images for products and restructuring with ejs

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,5 +5,6 @@ const indexController = require('../Controllers/indexController')
 
 /* / */
 router.get('/', indexController.index);
+router.get('/admin',indexController.admin );
 
 module.exports = router;
