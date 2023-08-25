a7e7a02ccbc299c1adf34bd714f5d6f390f0db49
Giselda  Arevalo <96034020+giselarevalo@users.noreply.github.com>
2023-08-25 18:38:44 -0300

    feat: category.ejs with products for category

--- src/Controllers/indexController.js
+++ src/Controllers/indexController.js
@@ -23,5 +23,25 @@ module.exports = {
 			keywords,
             products
 		})
-	}
+	},
+    category: (req, res) => {
+        const category = req.params.category;
+        let filteredProducts = products;
+
+
+        if (category !== 'todos') {
+            filteredProducts = products.filter(product => product.category.toLowerCase() === category.toLowerCase());
+        }
+        
+
+        return res.render('category', {
+            products,
+            filteredProducts,
+            category,
+            toThousand
+        });
+    }
+
 }
+
+
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const {index,admin,search} = require('../Controllers/indexController')
+const {index,admin,search, category} = require('../Controllers/indexController')
 
 /* / */
 router.get('/',index);
 router.get('/admin',admin );
-router.get('/search',search); 
+router.get('/search',search);
+router.get('/category/:category',category)
+
 
 
 
