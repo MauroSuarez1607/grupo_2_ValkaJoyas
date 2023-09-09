59feeb5fbc30188236af54158a31a07a408736fd
crissanguine <cris29sanguine@gmail.com>
2023-09-09 19:59:55 -0300

    add: cookie & remember

--- src/Controllers/users/processLogin.js
+++ src/Controllers/users/processLogin.js
@@ -5,15 +5,20 @@ module.exports = (req,res) => {
     const errors = validationResult(req)
 
     if(errors.isEmpty()){
-        const users = readJSON('users.json')
-        const user = users.find(user => user.email === req.body.email)
-        const {id, name, rol} = user
+        const users = readJSON('users.json');
+        const {email, remember} = req.body                     /* Ready: Add line cookie*/
+        const user = users.find(user => user.email === email);    /* Ready: Add line cookie*/
+        const {id, name, rol} = user;
 
         req.session.userLogin = {
             id,
             name,
             rol
         }
+
+        remember !== undefined && res.cookie('valkaJewels02',req.session.userLogin,{ 
+            maxAge : 1000 * 60 * 2 /* Ready: 2 minutos cookie*/
+        })
 
         return res.redirect('/')
     }else {
