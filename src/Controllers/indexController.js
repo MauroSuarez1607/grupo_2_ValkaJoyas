e9fd66c51582c0d49d2d0f38fa3a1d1198331fd2
Giselda  Arevalo <96034020+giselarevalo@users.noreply.github.com>
2023-10-23 16:08:52 -0300

    fix search to db

--- src/Controllers/indexController.js
+++ src/Controllers/indexController.js
@@ -1,28 +1,46 @@
 const db = require('../database/models');
+const { Op } = require('sequelize');
+
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
 module.exports = {
     index : (req,res) =>{
-
+        
         const valka = db.Product.findAll({
-			where:{
-				brandId: 1
-			}
-		});
-		const novedades = db.Product.findAll({
-			where:{
-				brandId: 2
-			}
-		});
-		Promise.all([valka,novedades])
-			.then(([valka,novedades])=>{
-				
-				return res.render('index',{
-					valka,
-					novedades,
-					toThousand
-				})
-			}).catch(error => console.log(error))
+            where: {
+                brandId: 1
+            },
+            include: {
+                model: db.Category,
+                as: 'category', // Alias configurado en el modelo
+                attributes: ['name']
+            },
+        });
+        
+        const novedades = db.Product.findAll({
+            where: {
+                brandId: 2
+            },
+            include: {
+                model: db.Category,
+                as: 'category', // Alias configurado en el modelo
+                attributes: ['name']
+            }
+        })
+        
+        Promise.all([valka, novedades])
+        
+            .then(([valkaResults, novedadesResults]) => {
+                console.log('Novedades:', novedadesResults);
+                return res.render('index', {
+                    valka: valkaResults,
+                    novedades: novedadesResults,
+                    toThousand
+                });
+            })
+            .catch(error => console.log(error));
+        
+        
     },
 
     admin : (req,res)=>{
@@ -49,39 +67,43 @@ module.exports = {
             .catch(error => console.log(error))
       
     },
-    search: (req, res) => {
-		/* const keywords = req.query.keywords
-		const results = products.filter(product=> product.name.toLowerCase().includes(keywords.toLowerCase()))
-		return res.render('results',{
-			results,
-			toThousand,
-			keywords,
-            products
-		}) */
-
-        const keywords = req.query.keywords
-
-		db.Product.findAll({
-			where:{
-				[Op.or]:[
-					{
-						name:{
-							[Op.substring]: keywords
-						}
-					}
-
-				]
-			}
-		})	
-			.then(results=>{
-				return res.render('results',{
-					results,
-					toThousand,
-					keywords
-				})
-			})
-			.catch(error => console.log(error))
-	},
+    search: async (req, res) => {
+        const keywords = req.query.keywords;
+    
+        try {
+            const results = await db.Product.findAll({
+                where: {
+                    [Op.or]: [
+                        {
+                            name: {
+                                [Op.substring]: keywords
+                            }
+                        }
+                    ]
+                }
+            });
+    
+            // Obtener productos para el slider
+            const productsForSlider = await db.Product.findAll({
+                where: {
+                    // Define la condición para los productos del slider
+                },
+                order: db.sequelize.random() 
+            });
+    
+            return res.render('results', {
+                results,
+                productsForSlider,  // Pasar productos para el slider
+                toThousand,
+                keywords,
+                products: results
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    },
+    
+    
 	
     category: (req, res) => {
         /* const category = req.params.category;
