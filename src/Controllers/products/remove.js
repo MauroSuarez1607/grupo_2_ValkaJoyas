2c9d0a215d390adde30aa2ed6cb3c7caf8ac1510
Giselda  Arevalo <96034020+giselarevalo@users.noreply.github.com>
2023-10-26 21:25:10 -0300

    feat remove to db

--- src/Controllers/products/remove.js
+++ src/Controllers/products/remove.js
@@ -1,31 +1,16 @@
-const { unlinkSync, existsSync } = require("fs");
-const { readJSON, writeJSON } = require("../../data")
+
+const db = require('../../database/models')
 
 module.exports = (req,res) => {
-    const products = readJSON('products.json')
-    const id = req.params.id
 
-    const productDelete = products.filter(product => product.id !== +id)
-
-    products.forEach(product => {
-        if (product.id === +id) {
-          if (product.image1) {
-            existsSync(`./src/public/images/${product.image1}`) &&
-              unlinkSync(`./src/public/images/${product.image1}`);
-          }
-      
-          if (product.image2 && product.image2.length > 0) {
-            product.image2.forEach(images => {
-              existsSync(`./src/public/images/${images}`) &&
-                unlinkSync(`./src/public/images/${images}`);
-            });
-          }
-        }
-      });
-
-// linea arriba para eliminar img de public
-    writeJSON(productDelete, 'products.json')
-
+  db.Product.destroy({
+    where : {
+      id: req.params.id
+    }
+  }).then(response=>{
+    console.log(response);
     return res.redirect('/admin')
+  })
+  .catch(error=> console.log(error))
 }
 
